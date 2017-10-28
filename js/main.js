02d847a7345fd7f2d2d1ea4dfdc684bbb03c8bbe
fredeeb <frederikbraendstrup@gmail.com>
2017-10-28 23:28:45 +0200

    Enlarged testfile

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let mohr = [];
 
 function setup() {
-  let c = createCanvas(630, 630);
+  let c = createCanvas(930, 930);
   c.id("squares");
 
   clear();
@@ -10,7 +10,7 @@ function setup() {
   for (var i = 0; i < 3; i++) {
     mohr[i] = [];
     for (var j = 0; j < 3; j++) {
-      mohr[i][j] = new mohrSquare (i * 210, j* 210, 5, 200, 20);
+      mohr[i][j] = new mohrSquare (i * 310, j* 310, 4, 300, 20);
     }
   }
   for (var i = 0; i < mohr[0].length; i++) {
